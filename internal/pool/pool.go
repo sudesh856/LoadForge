ffb107a60af92957c7bd9c86a51f6e5addee37b6
Sudesh <nooneandnothing09@gmail.com>
2026-03-24 21:40:54 +0545

    worker pool

---
+++ internal/pool/pool.go
@@ -0,0 +1,36 @@
+package pool
+
+import (
+	"context"
+
+	"github.com/sudesh856/LoadForge/internal/worker"
+)
+type Pool struct {
+	jobs chan worker.Job
+	results chan worker.Result
+}
+
+func New(bufferSize int) *Pool {
+	return &Pool{
+		jobs: make(chan worker.Job, bufferSize),
+		results: make(chan worker.Result, bufferSize),
+	}
+}
+
+
+func(p *Pool) Start(ctx context.Context, n int) {
+	for i := 0; i <n; i++ {
+		go worker.RunWorker(ctx, p.jobs, p.results)
+	}
+}
+func(p *Pool) Submit(job worker.Job) {
+	p.jobs <- job
+}
+
+func (p *Pool) Results() <-chan worker.Result {
+	return p.results
+}
+
+func (p *Pool) Close() {
+	close(p.jobs)
+}
---
+++ internal/pool/pool_test.go
@@ -0,0 +1,41 @@
+package pool
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"github.com/sudesh856/LoadForge/internal/worker"
+)
+
+func TestPool(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	defer server.Close()
+
+	ctx := context.Background()
+	p := New(100)
+	p.Start(ctx, 5)
+
+	for i := 0; i < 10; i++ {
+		p.Submit(worker.Job{URL: server.URL})
+	}
+
+	//collecting 10 results
+
+	for i := 0; i < 10; i++ {
+		result := <-p.Results()
+		if result.Err != nil {
+			 t.Errorf("Unexpected error: %v", result.Err)
+		}
+
+		if result.StatusCode != http.StatusOK {
+			t.Errorf("Expected 200, got %d", result.StatusCode)
+		}
+
+		t.Logf("Result %d -- status: %d latency: %v", i+1, result.StatusCode, result.Latency)
+
+	}
+}
